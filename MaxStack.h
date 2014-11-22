ef959e889c30fd7962a392e6271f1c08ef289a20
Philip Irri <philip@irri.se>
2014-11-22 14:50:30 +0900

    Splitted MaxStack into a header file and made it independent of std::stack implementation. MaxStack inherits from Stack and overrides pop and push, and implements getMax-method.

---
+++ MaxStack.h
@@ -0,0 +1,93 @@
+#include <stdexcept>
+
+template<typename T>
+class Node {
+public:
+  Node() {}
+
+  Node(T v) : Node() {
+    value = v;
+  }
+
+  Node* next = nullptr;
+  T value;
+};
+
+template<typename T>
+class Stack {
+public:
+  Stack() {}
+
+  Stack(T v) {
+    top = new Node<T>(v);
+  }
+
+  void push(T v) {
+    Node<T>* t = new Node<T>(v);
+    t->next = top;
+    top = t;
+  }
+
+  T pop() {
+    if (top == nullptr)
+      throw std::runtime_error("Stack is empty. Cannot pop.");
+
+    T v = top->value;
+    Node<T>* tmp = top;
+    top = top->next;
+
+    delete tmp;
+    return v;
+  }
+
+  T peek() {
+    if (top == nullptr)
+      throw std::runtime_error("Stack is empty. Cannot peek.");
+
+    return top->value;
+  }
+
+  bool empty() {
+    if (top == nullptr)
+      return true;
+    else
+      return false;
+  }
+
+
+private:
+  Node<T>* top = nullptr;
+};
+
+template<typename T>
+class MaxStack : public Stack<T> {
+public:
+  MaxStack() {
+
+  }
+
+  void push(T v) {
+  Stack<T>::push(v);
+  if (max_stack.empty() || v >= max_stack.peek())
+    max_stack.push(v);
+  }
+
+  T pop() {
+    T v = Stack<T>::pop();
+
+    if (v == max_stack.peek())
+      max_stack.pop();
+
+    return v;
+  }
+
+  T getMax() {
+    if (Stack<T>::empty())
+      throw std::runtime_error("Stack is empty. Cannot return max.");
+    return max_stack.peek();
+  }
+
+private:
+  Stack<T> max_stack;
+};
+
--- max_stack.cpp
+++ max_stack.cpp
@@ -1,83 +1,11 @@
+#include "MaxStack.h"
+
 #include <iostream>
-#include <stdexcept>
-#include <stack>
 using namespace std;
-
-template<typename T>
-class Node {
-public:
-  Node() {}
-
-  Node(T v) : Node() {
-    value = v;
-  }
-
-  Node* next = nullptr;
-  T value;
-};
-
-template<typename T>
-class MyStack {
-public:
-  MyStack() {}
-
-  MyStack(T v) {
-    top = new Node<T>(v);
-  }
-
-  void push(T v) {
-    Node<T>* t = new Node<T>(v);
-    t->next = top;
-    top = t;
-
-    if (max_list.empty() || v >= max_list.top())
-      max_list.push(v);
-  }
-
-  T pop() {
-    if (top == nullptr)
-      throw runtime_error("Stack is empty. Cannot pop.");
-
-    T v = top->value;
-    Node<T>* tmp = top;
-    top = top->next;
-
-    delete tmp;
-
-    if (v == max_list.top())
-      max_list.pop();
-
-    return v;
-  }
-
-  T peek() {
-    if (top == nullptr)
-      throw runtime_error("Stack is empty. Cannot peek.");
-
-    return top->value;
-  }
-
-  bool empty() {
-    if (top == nullptr)
-      return true;
-    else
-      return false;
-  }
-
-  T getMax() {
-    if (empty())
-      throw runtime_error("Stack is empty. Cannot return max.");
-    return max_list.top();
-  }
-
-private:
-  Node<T>* top = nullptr;
-  stack<T> max_list;
-};
 
 int main() {
 
-  MyStack<int> s;
+  MaxStack<int> s;
   s.push(1);
   cout << "Pushed " << s.peek() <<  " (max is " << s.getMax() << ")" << endl;
   s.push(2);
