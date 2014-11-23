0608a062105e65ede133084884a22546fa295848
Philip Irri <philip@irri.se>
2014-11-23 14:31:48 +0900

    finished brackets with ugly code.

--- bracket_validator.cpp
+++ bracket_validator.cpp
@@ -1,3 +1,5 @@
+// Warning! Ugly C code below.
+///////////////////////////////
 #include "MaxStack.h"
 #include <iostream>
 #include <string>
@@ -18,8 +20,9 @@ const int TYPE_INVALID = 0;
 const int TYPE_OPENER = 1;
 const int TYPE_CLOSER = 2;
 
-int isValidBracket(char c, Bracket* const brackets, unsigned int brackets_size) {
+int isValidBracket(char c, Bracket* const brackets, unsigned int brackets_size, Bracket& return_bracket) {
   for (int i=0; i < brackets_size; ++i) {
+    return_bracket = brackets[i];
     if (c == brackets[i].open)
       return TYPE_OPENER;
     if (c == brackets[i].close)
@@ -32,13 +35,14 @@ bool validateBrackets(const string& input, Bracket* const brackets, unsigned int
   Stack<char> openers;
 
   for (char c : input) {
-    int type = isValidBracket(c, brackets, brackets_size);
-    cout << c << " is " << ((type == 0) ? "not a " : (type == 1 ? "opener" : "closer")) << " bracket" << endl; 
+    Bracket the_bracket;
+    int type = isValidBracket(c, brackets, brackets_size, the_bracket);
+    //cout << c << " is " << ((type == 0) ? "not a " : (type == 1 ? "opener" : "closer")) << " bracket" << endl; 
 
     if (type == TYPE_OPENER)
       openers.push(c);
     else if (type == TYPE_CLOSER) {
-      if (openers.peek() != c)
+      if (openers.peek() != the_bracket.open)
         return false;
       openers.pop();
     }
@@ -57,12 +61,13 @@ int main() {
   brackets[2] = Bracket('[', ']');
 
   
-  const unsigned int INPUT_SIZE = 3;
+  const unsigned int INPUT_SIZE = 4;
   string input[INPUT_SIZE];
 
   input[0] = "{ [ ] ( ) }";
   input[1] = "{ [ ( ] ) }";
   input[2] = "{ [ }";
+  input[3] = "for(int i=0; i<10; i++) { cout << apa; };";
 
   for (int i=0; i < INPUT_SIZE; ++i) {
     bool result = validateBrackets(input[i], brackets, NR_BRACKETS);
